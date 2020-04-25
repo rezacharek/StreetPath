df9498698d48708e7e070fb71b61bd100635a24f
Roman Zacharek <romanzacharek@Romans-MacBook-Air.local>
2020-04-26 00:23:27 +0200

    Updating functions

--- path.cpp
+++ path.cpp
@@ -3,16 +3,16 @@
 #include <sstream>
 #include <fstream>
 #include <cstdlib>
+#include <algorithm>
 
 enum State
-{   kClosed, 
+{
+    kClosed,
     kEmpty,
     kObstacle
 };
 
-std::vector<std::vector<State> > Search(std::vector<std::vector<State> > Board, int array1[], int array2[]){
-    return Board;
-}
+
 
 std::string CellString(State state)
 {
@@ -35,10 +35,12 @@ std::vector<State> ParseLine(std::string line)
     for (int i = 0; i < 6; i++)
     {
         iss >> val;
-        if(val == 1){
+        if (val == 1)
+        {
             result.push_back(kObstacle);
         }
-        else{
+        else
+        {
             result.push_back(kEmpty);
         }
     }
@@ -70,26 +72,47 @@ std::vector<std::vector<State>> ReadBoardFile(std::string filePath)
     return result;
 }
 
-int Heuristic(int one, int two, int three, int four){
+int Heuristic(int one, int two, int three, int four)
+{
     return abs(three - one) + abs(four - two);
 }
 
-void AddToOpen(int x, int y, int g, int h, 
-                    std::vector<std::vector<int> > &OpenNodes,
-                    std::vector<std::vector<State> > &Grid){
-    std::vector<int> temp {x, y, g, h};
+void AddToOpen(int x, int y, int g, int h,
+               std::vector<std::vector<int>> &OpenNodes,
+               std::vector<std::vector<State>> &Grid)
+{
+    std::vector<int> temp{x, y, g, h};
     OpenNodes.push_back(temp);
     Grid[x][y] = kClosed;
 }
 
+std::vector<std::vector<State>> Search(std::vector<std::vector<State>> Board, int array1[2], int array2[2])
+{   
+    int x = array1[0];
+    int y = array1[1];
+    int g = 0;
+    int h = Heuristic(array1[0], array1[1], array2[0], array2[1]);
+    std::vector<std::vector<int> > Open {};
+    AddToOpen(x,y,g,h,Open,Board);
+
+    return Board;
+}
+
+bool Compare(std::vector<int> Node1, std::vector<int> Node2){
+    if(Node1[2] + Node1[3] < Node2[2] + Node2[3]){
+        return true;
+    }else{
+        return false;
+    }
+} 
 
 int main()
 {
-    std::vector<std::vector<State> > Board = ReadBoardFile("test_file.txt");
+    std::vector<std::vector<State>> Board = ReadBoardFile("test_file.txt");
     printBoard(Board);
-    int start[2] = {0,0};
-    int end[2] = {4,5};
-    std::vector<std::vector<State> > Solution = Search(Board, start, end);
+    int start[2] = {0, 0};
+    int end[2] = {4, 5};
+    std::vector<std::vector<State>> Solution = Search(Board, start, end);
     printBoard(Solution);
     return 0;
 }
