3cd1d359e24586c3a5577edc67130748b0323efe
Roman Zacharek <romanzacharek@Romans-MacBook-Air.local>
2020-04-26 00:03:58 +0200

    Preliminary functions for the A* algorithm

--- path.cpp
+++ path.cpp
@@ -2,12 +2,17 @@
 #include <vector>
 #include <sstream>
 #include <fstream>
+#include <cstdlib>
 
 enum State
-{
+{   kClosed, 
     kEmpty,
     kObstacle
 };
+
+std::vector<std::vector<State> > Search(std::vector<std::vector<State> > Board, int array1[], int array2[]){
+    return Board;
+}
 
 std::string CellString(State state)
 {
@@ -65,9 +70,26 @@ std::vector<std::vector<State>> ReadBoardFile(std::string filePath)
     return result;
 }
 
+int Heuristic(int one, int two, int three, int four){
+    return abs(three - one) + abs(four - two);
+}
+
+void AddToOpen(int x, int y, int g, int h, 
+                    std::vector<std::vector<int> > &OpenNodes,
+                    std::vector<std::vector<State> > &Grid){
+    std::vector<int> temp {x, y, g, h};
+    OpenNodes.push_back(temp);
+    Grid[x][y] = kClosed;
+}
+
+
 int main()
 {
-    printBoard(ReadBoardFile("test_file.txt"));
-
+    std::vector<std::vector<State> > Board = ReadBoardFile("test_file.txt");
+    printBoard(Board);
+    int start[2] = {0,0};
+    int end[2] = {4,5};
+    std::vector<std::vector<State> > Solution = Search(Board, start, end);
+    printBoard(Solution);
     return 0;
 }
