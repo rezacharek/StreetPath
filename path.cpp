7b88d9739898f3beba8c899db53ce1a482788ead
Roman Zacharek <romanzacharek@Romans-MacBook-Air.local>
2020-04-26 13:53:21 +0200

    Added some functionalities

--- path.cpp
+++ path.cpp
@@ -6,11 +6,16 @@
 #include <algorithm>
 
 enum State
-{
+{   
+    kPath,
     kClosed,
     kEmpty,
-    kObstacle
+    kObstacle,
+    kStart,
+    kFinish
 };
+
+const int Directions[4][2]{ {-1,0},{0,-1},{1,0},{0,1}};
 
 
 
@@ -86,7 +91,60 @@ void AddToOpen(int x, int y, int g, int h,
     Grid[x][y] = kClosed;
 }
 
-std::vector<std::vector<State>> Search(std::vector<std::vector<State>> Board, int array1[2], int array2[2])
+bool Compare(std::vector<int> Node1, std::vector<int> Node2){
+    if(Node1[2] + Node1[3] < Node2[2] + Node2[3]){
+        return true;
+    }else{
+        return false;
+    }
+} 
+
+void CellSort(std::vector<std::vector <int> > &Open){
+    sort(Open.begin(), Open.end(), Compare);
+}
+bool CheckValidCell(int x, int y, std::vector<std::vector<State> > &Board){
+    int colLength = Board.size();
+    int rowLength = Board[0].size();
+
+    if(y < 0 || y > rowLength - 1){
+        return false;
+    }
+    if(x < 0 || x > colLength - 1){
+        return false;
+    }
+
+    if(Board[x][y] == kEmpty){
+        return true;
+    }
+
+    return false;
+}
+
+void ExpandNeighbors(std::vector<int> &CurrentNode, 
+                        std::vector<std::vector<int> > &Open,
+                        std::vector<std::vector <State> > &Board,
+                        int goal[2]){
+    int x = CurrentNode[0];
+    int y = CurrentNode[1];
+    int g = CurrentNode[2];
+    int h = CurrentNode[3];
+
+    for(auto neighbour: Directions){
+        if(CheckValidCell(x + neighbour[0], y + neighbour[1], Board) == true){
+            AddToOpen(x + neighbour[0],
+                      x + neighbour[0],
+                      g + 1,
+                      Heuristic(x + neighbour[0],y + neighbour[1], goal[0], goal[1]),
+                      Open, 
+                      Board);
+
+        }
+    }
+}
+
+std::vector<std::vector<State>> Search(std::vector<std::vector<State>> Board
+                                , int array1[2]
+                                , int array2[2])
 {   
     int x = array1[0];
     int y = array1[1];
@@ -95,16 +153,24 @@ std::vector<std::vector<State>> Search(std::vector<std::vector<State>> Board, in
     std::vector<std::vector<int> > Open {};
     AddToOpen(x,y,g,h,Open,Board);
 
+    while(Open.size() > 0){
+        CellSort(Open);
+        std::vector<int> Current = Open[Open.size()-1];
+        Open.pop_back();
+
+        Board[Current[0]][Current[1]] = kPath;
+
+        if(Current[0] == array2[0] && Current[1] == array2[1]){
+            Board[array1[0]][array1[1]] = kStart;
+            Board[array2[0]][array2[1]] = kFinish;
+            return Board;
+        }
+        ExpandNeighbors(Current, Open, Board , array2);
+    }
     return Board;
 }
 
-bool Compare(std::vector<int> Node1, std::vector<int> Node2){
-    if(Node1[2] + Node1[3] < Node2[2] + Node2[3]){
-        return true;
-    }else{
-        return false;
-    }
-} 
+
 
 int main()
 {
